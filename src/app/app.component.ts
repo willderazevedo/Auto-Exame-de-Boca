3e57a1584fd53ef7a0907e85721111ee55772f48
Willder Azevedo <willderazevedo434@gmail.com>
2017-07-31 08:02:56 -0300

    feat(global): função do back button do hardware

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform, ModalController } from 'ionic-angular';
+import { Platform, ModalController, App, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -16,7 +16,8 @@ export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-  modalCtrl: ModalController) {
+  modalCtrl: ModalController, public app: App,
+  public alertCtrl: AlertController) {
     platform.ready().then(() => {
       let animatedSplash = modalCtrl.create(AnimatedSplashScreen);
 
@@ -24,5 +25,63 @@ export class MyApp {
       animatedSplash.present();
     });
   }
+<<<<<<< HEAD
+=======
+
+  /**
+   * Fix da tela branca na tela de load
+   * @param {SplashScreen} splashScreen Biblioteca nativa responsável pelo controle da tela de splash
+   */
+  private _hideSplashScreen(splashScreen) {
+    setTimeout(() => {
+      splashScreen.hide();
+    }, 100);
+  }
+
+  /**
+   * Método responsável por registrar botão de voltar do próprio celular
+   * @param   {Platform}      platform Biblioteca nativa responsável por conter as informações da plataforma atual
+   * @returns {void|boolean}
+   */
+  private _backButtonHardwareAction(platform) {
+    platform.registerBackButtonAction(() => {
+      let modalPortal   = this.app._appRoot._modalPortal.getActive();
+      let overlayPortal = this.app._appRoot._overlayPortal.getActive();
+
+      if(overlayPortal) {
+        overlayPortal.dismiss();
+
+        return false;
+      }
+
+      if(modalPortal) {
+        modalPortal.dismiss();
+
+        return false;
+      }
+
+      if(this.app.getActiveNav().canGoBack()) {
+        this.app.getRootNav().pop();
+
+        return false;
+      }
+
+      this.alertCtrl.create({
+        message: "Você deseja mesmo sair?",
+        buttons: [
+          {
+            text: "Não"
+          },
+          {
+            text: "Sim",
+            handler: () => {
+              platform.exitApp();
+            }
+          }
+        ]
+      }).present();
+    });
+  }
+>>>>>>> feat(global): função do back button do hardware
 }
 
