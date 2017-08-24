c2719ba7381cde9f0ee98a1ef606c4637d470399
Willder Azevedo <willderazevedo434@gmail.com>
2017-08-24 15:01:34 -0300

    fix(issues): fix de considerações

--- src/pages/exam/exam.ts
+++ src/pages/exam/exam.ts
@@ -20,12 +20,67 @@ export class ExamPage {
   showOkResult     = false;
   buttonText       = "Continuar";
   stepTitle        = "Passo 1 - Exame Extraoral";
-  actualExamPhoto  = 0;
-  defaultPhoto     = "./assets/images/default-image.jpg";
+  actualStep       = 0;
   tookPhoto        = "";
   examPhotos       = [
-    "./assets/images/exam/exam-1.jpg",
-    "./assets/images/exam/exam-2.jpg",
+    [
+      //Extraoral
+      {
+        image: "./assets/images/exam/extraoral/exam-1.jpg",
+        description: 'Em frente para o espelho, observe a pele do rosto e do pescoço e examine se há algum sinal que não tenha notado anteriormente.'
+      },
+      {
+        image: "./assets/images/exam/extraoral/exam-2.jpg",
+        description: 'Toque suavemente com as pontas dos dedos todo o rosto e procure por sinais e nódulos até então desconhecidos.'
+      },
+      {
+        image: "./assets/images/exam/extraoral/exam-3.jpg",
+        description: 'Observe também se o seu rosto está com os dois lados do mesmo tamanho.'
+      },
+      {
+        image: "./assets/images/exam/extraoral/exam-4.jpg",
+        description: 'Examine o pescoço. Compare os lados direito e esquerdo e veja se há diferenças entre eles. Depois, apalpe o lado esquerdo do pescoço com a mão direita. Repita o procedimento para o lado direito, palpando com a mão esquerda. Veja se existem caroços ou áreas endurecidas.'
+      },
+    ],
+    [
+      //Intraoral
+      {
+        image: "./assets/images/exam/intraoral/exam-1.jpg",
+        description: 'Puxe com os dedos o lábio superior, expondo a sua parte interna (mucosa), procure qualquer mudança de cor ou caroço ou ferida.'
+      },
+      {
+        image: "./assets/images/exam/intraoral/exam-2.jpg",
+        description: 'Puxe com os dedos toque na gengiva lábio superior e inferior, expondo a sua parte interna (mucosa), procure qualquer mudança de cor ou caroço ou ferida.'
+      },
+      {
+        image: "./assets/images/exam/intraoral/exam-3.jpg",
+        description: 'Com a ponta do dedo indicador, afaste a bochecha do lado direito para examinar a sua parte interna.'
+      },
+      {
+        image: "./assets/images/exam/intraoral/exam-4.jpg",
+        description: 'Com a ponta do dedo indicador, afaste a bochecha do lado esquerdo para examinar a sua parte interna.'
+      },
+      {
+        image: "./assets/images/exam/intraoral/exam-5.jpg",
+        description: 'Puxe com os dedos o lábio inferior, expondo a sua parte interna (mucosa), procure qualquer mudança de cor ou caroço ou ferida.'
+      },
+      {
+        image: "./assets/images/exam/intraoral/exam-6.jpg",
+        description: 'Incline a cabeça para trás e, abrindo a boca o máximo possível, examine atentamente o céu da boca.'
+      },
+      {
+        image: "./assets/images/exam/intraoral/exam-7.jpg",
+        description: 'Incline a cabeça para trás e, abrindo a boca o máximo possível, examine atentamente o céu da boca. Palpe com o dedo indicador todo o céu da boca. Em seguida, diga ÁÁÁÁ... e observe o fundo da garganta.'
+      },
+      {
+        image: "./assets/images/exam/intraoral/exam-8.jpg",
+        description: 'Introduza o dedo indicador por baixo da língua e o polegar da mesma mão por baixo do queixo e procure palpar todo o assoalho da boca.'
+      },
+      {
+        image: "./assets/images/exam/intraoral/exam-9.jpg",
+        description: 'Ponha a língua para fora e observe a parte de cima. Repita a observação com a língua levantada até o céu da boca. Em seguida, puxando a língua para a esquerda, observe o lado esquerdo da mesma. Repita o procedimento para o lado direito.'
+      },
+    ],
   ];
   questionsKeys    = [
     "questionOne", 
@@ -73,15 +128,15 @@ export class ExamPage {
     }
 
     this.savedQuestions.push(choosedClone);
-    this.actualExamPhoto++;
+    this.actualStep++;
     this._uncheckFields();
 
-    if(this.actualExamPhoto === 1) {
+    if(this.actualStep === 1) {
       this.buttonText = "Finalizar autoexame";
       this.stepTitle  = "Passo 2 - Exame Intraoral"; 
     }
 
-    if(this.actualExamPhoto === 2) {
+    if(this.actualStep === 2) {
       this._calculateResult();
 
       return false;
@@ -89,7 +144,7 @@ export class ExamPage {
 
     this.toastCtrl.create({
       position: "bottom",
-      message:  "Etapa " + (this.actualExamPhoto) + " de 2 concluída",
+      message:  "Etapa " + (this.actualStep) + " de 2 concluída",
       duration: 1500
     }).present();
 
@@ -155,7 +210,7 @@ export class ExamPage {
 
   public doItAgain() {
     this.savedQuestions.splice(0);
-    this.actualExamPhoto = 0;
+    this.actualStep = 0;
     this.buttonText      = "Continuar";
     this.tookPhoto       = "";
     this.hideExam        = false;
